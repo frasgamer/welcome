92ee68dc8076eab96b89d15d190e469da0cf3abd
frasgamer <44925264+frasgamer@users.noreply.github.com>
2018-11-29 21:11:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("514873591754522671");
+const mohamed= member.guild.channels.get("516267367714783237");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`welcome in the server [user] :heart:`), 4000)        
+setTimeout(() => mohamed.send(`welcome in the server [user] [member] :heart:`), 4000)        
 }
 });
 
